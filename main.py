87be88b2b90117849d07385db64a9afa631b3caf
Harshil2107 <harshilp2107@gmail.com>
2022-07-06 03:02:53 -0700

    organized in functions

--- main.py
+++ main.py
@@ -4,21 +4,27 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import requests
 from bs4 import BeautifulSoup
-r= requests.get('https://www.vlr.gg/matches')
 
-soup = BeautifulSoup(r.content, 'html.parser')
-s = soup.find_all('div', class_= 'ml mod-live')
-live_matchs =[]
+def getLiveMatches():
+    r = requests.get('https://www.vlr.gg/matches')
 
-for i in s:
-    d = i.parent.parent.findChildren('div', class_="match-item-vs-team-name", recursive=True)
-    match = {}
-    match['link'] = "https://www.vlr.gg" + i.parent.parent.get('href')
-    i = 1
-    for teams in d:
-        match['team'+str(i)] = teams.findChild('div', class_='text-of').text.split()[0]
-        i+=1
-    live_matchs.append(match)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    s = soup.find_all('div', class_='ml mod-live')
+    live_matchs = []
 
-for i in live_matchs:
-    print(i)
+    for i in s:
+        d = i.parent.parent.findChildren('div', class_="match-item-vs-team-name", recursive=True)
+        match = {}
+        match['link'] = "https://www.vlr.gg" + i.parent.parent.get('href')
+        i = 1
+        for teams in d:
+            match['team' + str(i)] = teams.findChild('div', class_='text-of').text.split()[0]
+            i += 1
+        live_matchs.append(match)
+    return live_matchs
+
+def getMatchScore(match_data):
+    print(match_data)
+
+live = getLiveMatches()
+getMatchScore(live[0])
